521dc6a5248a6288dbfb737db19e54cbb8bf56a8
iiiroha <subkamiya.kohsuke@gmail.com>
2025-08-28 13:32:48 +0900

    language-tag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,40 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { LanguageTag } from "@/components/language-tag";
 
-const Home = () => {
-  return (
-    <>
-      <h1>ホームページ</h1>
-      <Link href="/signin">サインインページへ</Link>
-      <Button>shadcn/ui test</Button>
-    </>
-  );
+import React,{useState} from "react";
+import { Plus, X } from "lucide-react";
+
+
+  const languages:string[] = ["ai", "cloud", "elixer","python","react","java","gaming", "javascript",
+  ".net", "mobile", "testing", "tools", "data-science", "architecture", "crypto",
+  "security", "database", "machine-learning", "webdev", "rust", "ruby", "devops",
+  "open-source", "golang", "tech-news", "nodejs", "npm", "typescript"];
+
+  export default function App(){
+  const[selected, setSelected]=useState<{[Key:string]:boolean}>({});
+
+  const toggle = (language:any) =>{
+    setSelected((prev)=>({...prev,[language]:!prev[language]}));
 };
+  
+  return (
+    <div className="flex flex-wrap gap-2 p-4 bg black text-sm font-medium">
+      {languages.map((language) => (
+        <button key={language} 
+        onClick={() => toggle(language)}
+        className={`px-4 py-2 rounded-lg border transition
+            ${selected[language] ? "bg-white text-black" : "bg-black text-white border-white"}`}
+        >
+          {language}
+          <span className="ml-1 text">
+          {selected[language]?"×":"+"}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+}
 
-export default Home;
+
--- frontend/src/components/language-tag.tsx
+++ frontend/src/components/language-tag.tsx
@@ -1,1 +1,16 @@
-oooooo
+type LanguageTagProps = {
+    isSelected:boolean;
+    name:string;
+    onClicked: () => void;
+};
+
+export const LanguageTag = ({isSelected,name,onClicked}: LanguageTagProps) => {
+    return (
+        <button
+        onClick={onClicked} 
+        className={`px-3 py-1 rounded border transition 
+            ${isSelected?"bg-white text-black":"bg-black text-white"}`}>
+            {name}{isSelected?"×":"+"}
+        </button>
+    );
+};
