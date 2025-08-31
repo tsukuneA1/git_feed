776d572aca4b8629a71482c20261d9f8baba44bd
HirotoIkeda04 <hiroto.lalapalooza.ikeda@gmail.com>
2025-08-31 13:41:47 +0900

    エラーを修正

--- frontend/src/components/userIcon.tsx
+++ frontend/src/components/userIcon.tsx
@@ -1,20 +1,19 @@
-import React, {ReactNode} from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
 interface userIconProps {
-    source : string
+  source: string;
 }
 
-const userIcon = ({source} : userIconProps) => {
-    return (
-        <Image
-        src={source}
-        alt="Image of userIcon" 
-        width={100}
-        height={100}
-        className="rounded-full"
-        />
-    )
-}
+const userIcon = ({ source }: userIconProps) => {
+  return (
+    <Image
+      src={source}
+      alt="Image of userIcon"
+      width={100}
+      height={100}
+      className="rounded-full"
+    />
+  );
+};
 
-export default userIcon
+export default userIcon;
