cc4b702af77b86b7c67b3215d9faaae2ba0085e4
HirotoIkeda04 <hiroto.lalapalooza.ikeda@gmail.com>
2025-08-31 12:58:34 +0900

    made userIcon

---
+++ frontend/src/components/userIcon.tsx
@@ -0,0 +1,12 @@
+import React, {ReactNode} from "react";
+
+const userIcon = () => {
+    return (
+        <img 
+        src="frontend/public/vercel.svg"
+        alt="Image of userIcon" 
+        />
+    )
+}
+
+export default userIcon
