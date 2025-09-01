cf48b2879028b968159fb7e5a75c7e7b56f1cc19
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-01 10:54:50 +0900

    copilotに指摘された箇所を修正

--- backend/db/migrate/20250901010206_create_tags.rb
+++ backend/db/migrate/20250901010206_create_tags.rb
@@ -3,8 +3,7 @@ class CreateTags < ActiveRecord::Migration[8.0]
     create_table :tags, id: :uuid do |t|
       t.string :slug, null: false
       t.string :label, null: false
-      t.timestamp :created_at, null: false, default: -> {"CURRENT_TIMESTAMP"}
-      
+      t.timestamps created_at: true, updated_at: false
     end
     add_index :tags, :slug, unique: true
   end
