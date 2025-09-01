7c8ca9dc6f6425f8fc4844b767464e098fa53a48
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-01 10:31:33 +0900

    UUIDを採用し、制作日時を自動セットするように修正

--- backend/db/migrate/20250901010206_create_tags.rb
+++ backend/db/migrate/20250901010206_create_tags.rb
@@ -1,11 +1,10 @@
 class CreateTags < ActiveRecord::Migration[8.0]
   def change
-    create_table :tags do |t|
-      t.string :slug
-      t.string :label
-      t.timestamp :created_at
-
-      t.timestamps
+    create_table :tags, id: :uuid do |t|
+      t.string :slug, null: false
+      t.string :label, null: false
+      t.timestamp :created_at, null: false, default: -> {"CURRENT_TIMESTAMP"}
+      
     end
     add_index :tags, :slug, unique: true
   end
