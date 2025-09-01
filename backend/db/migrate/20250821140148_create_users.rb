68694c578c8b86418c65114797e5ae61cf66133b
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-01 10:37:58 +0900

    github_events テーブル用のマイグレーション雛形の微修正

--- backend/db/migrate/20250821140148_create_users.rb
+++ backend/db/migrate/20250821140148_create_users.rb
@@ -1,16 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: :uuid do |t|
       t.string :provider, null: false, default: "github"
       t.string :uid, null: false
       t.string :username, null: false
       t.string :name
       t.string :avatar_url
+
       t.datetime :last_login_at
       t.inet :last_login_ip
       t.string :last_login_user_agent
 
-      t.timestamps
+      t.timestamps null: false
     end
 
     add_index :users, [ :provider, :uid ], unique: true
