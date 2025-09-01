c5d3360acc1139f3db49511e7a2192ef3c7b1a83
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-01 18:57:22 +0900

    しゅ末い

--- backend/db/migrate/20250901010206_create_tags.rb
+++ backend/db/migrate/20250901010206_create_tags.rb
@@ -1,9 +1,11 @@
 class CreateTags < ActiveRecord::Migration[8.0]
   def change
-    create_table :tags, id: :uuid do |t|
-      t.string :slug, null: false
-      t.string :label, null: false
-      t.timestamps created_at: true, updated_at: false
+    create_table :tags do |t|
+      t.string :slug
+      t.string :label
+      t.timestamp :created_at
+
+      t.timestamps
     end
     add_index :tags, :slug, unique: true
   end
--- backend/db/migrate/20250901010551_create_github_events.rb
+++ backend/db/migrate/20250901010551_create_github_events.rb
@@ -3,8 +3,8 @@ class CreateGithubEvents < ActiveRecord::Migration[8.0]
     create_table :github_events do |t|
       t.string :remote_event_id
       t.string :event_type
-      t.timestamp :event_time
-      t.timestamp :fetched_at
+      t.datetime :event_time
+      t.datetime :fetched_at
       t.integer :actor_remote_id
       t.integer :repo_remote_id
       t.string :language_slug
