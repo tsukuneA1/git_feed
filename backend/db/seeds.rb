9b844af402e1a4b1f919e92dee19e83939bafc9b
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-02 13:21:26 +0900

    seedを書き中

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+base = Rails.root.join("vendor/lingulst")
+languages_yml = YAML.sale_load_fule(base.join("languages.yml"))
+popular_set = YAML.sale_load_file(base.join("popular.yml")).map(&:to_s).to_set
+
+def slugify(name)
+    name.downcase
+        .gsub("++", "plus-plus")
+        .gsub("#", "sharp")
+        .gsub(/\s+/, "-")
+        .gsub(/[^a-z0-9\-]/, "")
+end
