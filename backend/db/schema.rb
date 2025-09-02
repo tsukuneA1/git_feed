ed6800ac0b5a30079e34dfca3d305285789852c7
tsukuneA1 <tkschr1125@gmail.com>
2025-09-02 14:32:46 +0900

    migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_02_045117) do
   create_table "github_events", force: :cascade do |t|
     t.string "remote_event_id"
     t.string "event_type"
-    t.datetime "event_time", precision: nil
-    t.datetime "fetched_at", precision: nil
+    t.datetime "event_time"
+    t.datetime "fetched_at"
     t.integer "actor_remote_id"
     t.integer "repo_remote_id"
     t.string "language_slug"
