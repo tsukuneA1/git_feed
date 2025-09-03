9b34dab780d0e764a039ee53369dc75484c8503c
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-03 20:38:14 +0900

    リクエストごとにcurrent_user をDBに伝えるように書き直し

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  around_action :with_db_rls_user
+
+  private
+
+  def with_db_rls_user
+    ActiveRecord::Base.connection.transaction(requires_new: true) do
+      set_db_current_user(current_user&.id)
+      yield
+    end
+  end
+
+  def set_db_current_user(user_id)
+    if user_id
+      ActiveRecord::Base.connection.execute(
+        "SET LOCAL app.current_user_id = #{Integer(user_id)}"
+      )
+     else
+      ActiveRecord::Base.connection.execute(
+        "SET LOCAL app.current_user_id = NULL"
+      )
+    end
+  end
 end
