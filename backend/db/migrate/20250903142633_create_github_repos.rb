963df5d632df79ff3bb0cd794d390d412bc0ddff
SaeYoshizaki <yoshizakisae@gmail.com>
2025-09-04 00:00:33 +0900

    テーブルを作った

--- backend/db/migrate/20250903142633_create_github_repos.rb
+++ backend/db/migrate/20250903142633_create_github_repos.rb
@@ -1,7 +1,15 @@
 class CreateGithubRepos < ActiveRecord::Migration[8.0]
   def change
-    create_table :github_repos do |t|
-      t.timestamps
+    create_table :github_repos, id: :uuid do |t|
+      t.references :user, null: false, foreign_key: true, type: :uuid, index: true
+
+      t.bigint :repo_id, null: false
+      t.string :name, null: false
+      t.string  :html_url
+      t.text    :description
+      t.string  :language
+      t.integer :stargazers_count, null: false, default: 0
+      t.integer :forks_count,      null: false, default: 0
     end
   end
 end
